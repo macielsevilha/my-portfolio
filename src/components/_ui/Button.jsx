64f0fd7d30920561a4718df7897d41e173a00a23
Maciel <dev.macielsevilha@example.com>
2023-11-28 12:02:59 -0300

    atualizando build

--- src/components/_ui/Button.jsx
+++ src/components/_ui/Button.jsx
@@ -2,35 +2,10 @@
 
 import React from 'react';
 import styles from './Button.module.scss';
-import { useState } from 'react';
-
-
-
-
-const Headers = () => {
- 
-}
-function Sobre() {
-  
-}
-function Experiencia() {
- 
-}
-function Habilidade() {
- 
-}
 
 const Button = ({ className, children, onClick }) => {
-  
-  const [activeSection, setActiveSection] = useState('sobre');
-
-  const showSection = (section) => {
-    setActiveSection(section);
-  };
-  
-
   return (
-    <button className={`${styles.button} ${styles[className]}`} onClick={() => showSection(onClick)}>
+    <button className={`${styles.button} ${styles[className]}`} onClick={onClick}>
       {children}
     </button>
   );
